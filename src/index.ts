be7c80cc856be8f68dc62caf712d3ede794df852
Harry Hartley <hhartley94@gmail.com>
2024-05-13 16:42:00 +0100

    cache requests and return the correct data

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,11 @@ app.use(express.json())
 
 const opensky = 'https://opensky-network.org/api/flights'
 
+// const testStrings = [
+//   'http://localhost:3000/aircraft?type=arrival&airport=EDDF&begin=1517227200&end=1517230800',
+//   'http://localhost:3000/aircraft?type=departure&airport=EDDF&begin=1517227200&end=1517230800',
+// ]
+
 type OpenSkyResponse = {
   icao24: string
   firstSeen: number
@@ -24,26 +29,31 @@ type OpenSkyResponse = {
   arrivalAirportCandidatesCount: number
 }
 
-// Get all items
 app.get('/aircraft', async (req: Request, res: Response) => {
   try {
-    console.log(req.query)
-
     const cachedRequest = await prisma.request.findFirst({
       where: {
-        type: req.query.type as string,
+        type: req.query.type as 'arrival' | 'departure',
         airport: req.query.airport as string,
         begin: Number.parseInt(req.query.begin as string),
         end: Number.parseInt(req.query.end as string),
       },
       include: {
-        aircraft: true,
+        aircraft: {
+          select: {
+            icao24: true,
+            estDepartureAirport: true,
+            estArrivalAirport: true,
+            callsign: true,
+            firstSeen: true,
+            lastSeen: true,
+          },
+        },
       },
     })
 
     if (cachedRequest) {
-      res.status(200).json(cachedRequest.aircraft)
-      return
+      return res.status(200).json(cachedRequest.aircraft)
     }
 
     const aircraft = await fetch(
@@ -51,6 +61,15 @@ app.get('/aircraft', async (req: Request, res: Response) => {
     )
 
     const aircraftPayload: OpenSkyResponse[] = await aircraft.json()
+
+    const aircraftPayloadNormalised = aircraftPayload.map((aircraft) => ({
+      icao24: aircraft.icao24,
+      estDepartureAirport: aircraft.estDepartureAirport,
+      estArrivalAirport: aircraft.estArrivalAirport,
+      callsign: aircraft.callsign,
+      firstSeen: aircraft.firstSeen,
+      lastSeen: aircraft.lastSeen,
+    }))
 
     await prisma.request.create({
       data: {
@@ -60,94 +79,17 @@ app.get('/aircraft', async (req: Request, res: Response) => {
         end: Number.parseInt(req.query.end as string),
         aircraft: {
           createMany: {
-            data: aircraftPayload.map((aircraft) => ({
-              icao24: aircraft.icao24,
-              estDepartureAirport: aircraft.estDepartureAirport,
-              estArrivalAirport: aircraft.estArrivalAirport,
-              callsign: aircraft.callsign,
-              firstSeen: aircraft.firstSeen,
-              lastSeen: aircraft.lastSeen,
-            })),
+            data: aircraftPayloadNormalised,
           },
         },
       },
     })
 
-    res.status(200).json(aircraft)
+    return res.status(200).json(aircraftPayloadNormalised)
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' })
+    return res.status(500).json({ error: 'Internal server error' })
   }
 })
-
-// // Create a new item
-// app.post("/items", async (req: Request, res: Response) => {
-//     const { name, description } = req.body;
-//     try {
-//         const newItem = await prisma.item.create({
-//             data: {
-//                 name,
-//                 description,
-//             },
-//         });
-//         res.status(201).json(newItem);
-//     } catch (error) {
-//         res.status(500).json({ error: "Internal server error" });
-//     }
-// });
-
-// // Get a single item
-// app.get("/items/:id", async (req: Request, res: Response) => {
-//     const id = parseInt(req.params.id!);
-//     try {
-//         const item = await prisma.item.findUnique({
-//             where: {
-//                 id,
-//             },
-//         });
-//         if (!item) {
-//             res.status(404).json({ error: "Item not found" });
-//         } else {
-//             res.json(item);
-//         }
-//     } catch (error) {
-//         res.status(500).json({ error: "Internal server error" });
-//     }
-// });
-
-// // Delete an item
-// app.delete("/items/:id", async (req: Request, res: Response) => {
-//     const id = parseInt(req.params.id!);
-//     try {
-//         await prisma.item.delete({
-//             where: {
-//                 id,
-//             },
-//         });
-//         res.sendStatus(204);
-//     } catch (error) {
-//         res.status(500).json({ error: "Internal server error" });
-//     }
-// });
-
-// // Update an item
-// app.put("/items/:id", async (req: Request, res: Response) => {
-//     const id = parseInt(req.params.id!);
-//     const { name, description } = req.body;
-//     try {
-//         const updatedItem = await prisma.item.update({
-//             where: {
-//                 id,
-//             },
-//             data: {
-//                 name,
-//                 description,
-//             },
-//         });
-//         res.json(updatedItem);
-//     } catch (error) {
-//         res.status(500).json({ error: "Internal server error" });
-//     }
-// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
